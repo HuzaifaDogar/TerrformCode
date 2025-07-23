de07d3f62680d140516ddc3ae58e02c8ce8c0383
Huzaifa Dilshad <127135503+HuzaifaDogar@users.noreply.github.com>
2025-07-23 21:57:58 +0500

    Add files via upload

---
+++ EC2.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "terraform-generated-key-${random_id.instance.hex}"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "random_id" "instance" {
+  byte_length = 4
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.generated_key.key_name
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.example.private_key_pem
+  sensitive = true
+}
